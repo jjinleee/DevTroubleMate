00b944733fafb4cbf1e228a76b2058541933d7da
이진 <ijin@ijin-ui-MacBookAir.local>
2026-06-21 16:38:35 +0900

    init: create spring boot project

---
+++ src/main/java/com/jinlee/devtroublemate/DevtroublemateApplication.java
@@ -0,0 +1,13 @@
+package com.jinlee.devtroublemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevtroublemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevtroublemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jinlee/devtroublemate/DevtroublemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinlee.devtroublemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevtroublemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
